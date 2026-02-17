79b4a0116622caa7fb1dc931e9e89d4137d977cb
GYRO-TIGER <dhruv98110sharma@gmail.com>
2026-02-17 10:22:16 +0530

    first commit

---
+++ run.ps1
@@ -0,0 +1,2 @@
+## Running the code without wirting the passing parameters manually just need to update them here!!
+python main.py -i edited1.mp4 -o output_vertical.mp4
---
+++ run2.ps1
@@ -0,0 +1,2 @@
+python hyperfollow.py edited1.mp4 track --preview
+python hyperfollow.py edited1.mp4 reframe --preview
